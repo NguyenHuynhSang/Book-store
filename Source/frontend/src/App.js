e733d9e08be3059d76e57edf35feaefcb27ad93f
Nguyễn Huỳnh Sáng <16521022@gm.uit.edu.vn>
2022-11-14 17:19:07 +0700

    add product page

--- Source/frontend/src/App.js
+++ Source/frontend/src/App.js
@@ -1,10 +1,28 @@
+import data from './tempdata';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Shop ban hang</a>
       </header>
-      <main>List Product</main>
+      <main>
+        <h1>Products</h1>
+        <div className="products">
+          {data.product.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name}></img>
+              </a>
+              <div className="product-infor">
+                <p>{product.name}</p>
+                <p>VND {product.price}</p>
+                <button>Mua hang</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ Source/frontend/src/tempdata.js
@@ -0,0 +1,38 @@
+const data = {
+  product: [
+    {
+      name: 'nike',
+      slug: 'nike-shirt',
+      image: '/imgs/n1.jpg',
+      price: '2000000',
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 5,
+      numRev: 10,
+      des: 'Good stuff',
+    },
+    {
+      name: 'nike2',
+      slug: 'nike-shirt2',
+      image: '/imgs/n2.jpg',
+      price: '2000000',
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 5,
+      numRev: 10,
+      des: 'Good stuff',
+    },
+    {
+      name: 'nike3',
+      slug: 'nike-shirt3',
+      image: '/imgs/n3.jpg',
+      price: '2000000',
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 5,
+      numRev: 10,
+      des: 'Good stuff',
+    },
+  ],
+};
+export default data;
