63f024e0de513c584db2930d121b34b9e6c2c582
Nguyễn Huỳnh Sáng <16521022@gm.uit.edu.vn>
2022-11-21 08:38:08 +0700

    fix minor

--- Source/frontend/src/App.js
+++ Source/frontend/src/App.js
@@ -8,15 +8,16 @@ function App() {
       </header>
       <main>
         <h1>Products</h1>
-        <div className="products">
-          {data.product.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name}></img>
+        <div className="books">
+          {data.books.map((book) => (
+            <div className="book" key={book.slug}>
+              <a href={`/product/${book.slug}`}>
+                <img src={book.image} alt={book.name}></img>
               </a>
-              <div className="product-infor">
-                <p>{product.name}</p>
-                <p>VND {product.price}</p>
+              <div className="book-infor">
+                <p>{book.name}</p>
+                <p>VND {book.price}</p>
+                <br></br>
                 <button>Mua hang</button>
               </div>
             </div>
