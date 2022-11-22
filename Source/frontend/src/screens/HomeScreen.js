ce23f57a82c074d5057bb7702e1704f09902ed2e
Nguyễn Huỳnh Sáng <16521022@gm.uit.edu.vn>
2022-11-22 14:11:02 +0700

    add reducer hook to manage states

--- Source/frontend/src/screens/HomeScreen.js
+++ Source/frontend/src/screens/HomeScreen.js
@@ -1,13 +1,38 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQ':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, books: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
 function HomeScreen() {
-  const [books, setBooks] = useState([]);
+  const [{ loading, error, books }, dispatch] = useReducer(reducer, {
+    books: [],
+    loading: true,
+    error: '',
+  });
+  // const [books, setBooks] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('api/books');
-      setBooks(result.data);
+      dispatch({ type: 'FETCH_REQ' });
+      try {
+        const result = await axios.get('api/books');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+        // setBooks(result.data);
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
     };
     fetchData();
   }, []);
