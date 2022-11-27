b9615232297c1f9dc77adf95531b619c1910b80a
Nguyễn Huỳnh Sáng <16521022@gm.uit.edu.vn>
2022-11-27 14:12:39 +0700

    add react-bootstrap, remove swagger

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,9 @@
 import express from 'express';
 import data from './tempdata.js';
-import swaggerUi from 'swagger-ui-express';
-import swaggerJsDoc from 'swagger-jsdoc';
+
 const app = express();
-// app.use(express.json());
+app.use(express.json());
 
-const swaggerOption = {
-  swaggerDefinition: {
-    info: {
-      title: 'Doc',
-      description: 'api doc',
-      contact: {
-        name: 'NHS',
-      },
-      server: 'http://localhost:5000',
-    },
-  },
-  apis: ['server.js'],
-};
-const swaggerDocument = swaggerJsDoc(swaggerOption);
-
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// Routes
-/**
- * @swagger
- * /Books
- * get
- *  description: bla bla bla
- *  responses:
- *    '200':
- *      description: done
- */
 app.get('/api/books', (req, res) => {
   res.send(data.books);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,34 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import BookScreen from './screens/BookScreen';
 import HomeScreen from './screens/HomeScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Books</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
           <Link to="/">Book</Link>
         </header>
         <main>
-          <Routes>
-            <Route path="/" element={<HomeScreen />}></Route>
-            <Route path="/book/:slug" element={<BookScreen />}></Route>
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />}></Route>
+              <Route path="/book/:slug" element={<BookScreen />}></Route>
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">footer</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
