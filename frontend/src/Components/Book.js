6329fcaa519eae28d0fac953f0f59920a65112d3
Nguyễn Huỳnh Sáng <16521022@gm.uit.edu.vn>
2022-11-27 15:14:50 +0700

    add rating component

--- frontend/src/Components/Book.js
+++ frontend/src/Components/Book.js
@@ -1,6 +1,8 @@
 import { Button } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
+import Rating from './Rating';
+
 function Book(props) {
   const { book } = props;
   return (
@@ -9,6 +11,7 @@ function Book(props) {
         <img src={book.image} className="cart-img-top" alt={book.name}></img>
       </Link>
       <Card.Body>
+        <Rating rating={book.rating} numReviews={book.numReviews}></Rating>
         <Card.Title>{book.name}</Card.Title>
         <Card.Text>{book.price}</Card.Text>
       </Card.Body>
